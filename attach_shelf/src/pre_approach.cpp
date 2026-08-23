75c5a22a2280975688d637260f2e9074126fba2d
Krishna Mohan <mohankri@gmail.com>
2026-08-23 20:00:21 +0000

    fix package name

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -1,3 +1,5 @@
+#include "rcl_interfaces/msg/detail/parameter_descriptor__struct.hpp"
+#include "rclcpp/logger.hpp"
 #include "rclcpp/logging.hpp"
 #include "rclcpp/qos_overriding_options.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -12,6 +14,22 @@ class PreApproach : public rclcpp_lifecycle::LifecycleNode {
 public:
   PreApproach() : rclcpp_lifecycle::LifecycleNode("PreApproachNode") {
     RCLCPP_INFO(this->get_logger(), "Pre Approach");
+
+    /* parameter descriptor */
+    rcl_interfaces::msg::ParameterDescriptor obstacle_desc;
+    obstacle_desc.description = "Obstacle distance in meter.";
+
+    rcl_interfaces::msg::ParameterDescriptor degrees_desc;
+    degrees_desc.description = "Number of degree of rotation";
+
+    /* declare decriptor */
+    this->declare_parameter<double>("obstacle", 0.0, obstacle_desc);
+    this->declare_parameter<double>("degrees", 0.0, degrees_desc);
+
+    RCLCPP_INFO(this->get_logger(), "Obstacle Distance %f",
+                this->get_parameter("obstacle").as_double());
+    RCLCPP_INFO(this->get_logger(), "Degrees %f",
+                this->get_parameter("degrees").as_double());
   }
 
 protected:
