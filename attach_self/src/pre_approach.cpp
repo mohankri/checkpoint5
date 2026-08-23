4e8cb1259bd653036acf64397a7a784cc8fdb5ea
Krishna Mohan <mohankri@gmail.com>
2026-08-23 19:13:15 +0000

    Managed Node Lifecycle skelton

---
+++ attach_self/src/pre_approach.cpp
@@ -0,0 +1,54 @@
+#include "rclcpp/logging.hpp"
+#include "rclcpp/qos_overriding_options.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include <cstdint>
+#include <memory>
+
+using CallbackReturn =
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+class PreApproach : public rclcpp_lifecycle::LifecycleNode {
+public:
+  PreApproach() : rclcpp_lifecycle::LifecycleNode("PreApproach") {
+    RCLCPP_INFO(this->get_logger(), "Pre Approach");
+  }
+
+protected:
+  CallbackReturn on_configure(rclcpp_lifecycle::State &) {
+    RCLCPP_INFO(this->get_logger(), "On Configure");
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn on_activate(rclcpp_lifecycle::State &) {
+    RCLCPP_INFO(this->get_logger(), "On Activate");
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn on_deactivate(rclcpp_lifecycle::State &) {
+    RCLCPP_INFO(this->get_logger(), "On Deactivate");
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn on_cleanup(rclcpp_lifecycle::State &) {
+    RCLCPP_INFO(this->get_logger(), "On Cleanup");
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn on_shutdown(rclcpp_lifecycle::State &) {
+    RCLCPP_INFO(this->get_logger(), "On Shutdown");
+    return CallbackReturn::SUCCESS;
+  }
+
+  CallbackReturn on_error(rclcpp_lifecycle::State &) {
+    RCLCPP_INFO(this->get_logger(), "On Error");
+    return CallbackReturn::SUCCESS;
+  }
+};
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<PreApproach>();
+  rclcpp::spin(node->get_node_base_interface());
+  rclcpp::shutdown();
+}
